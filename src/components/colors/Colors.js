59dbf3bc5332698214cf7da88db0008418429ab6
Tamas Balint <balint.tamas87@gmail.com>
2018-03-25 15:30:39 +0100

    Include Color component in Colors component
    Display/Render all colors with their correspondent hex value

--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Colors.css";
+import Color from "../color/Color";
+
+const colors = ["111111", "2F80ED", "56CCF2", "f3f2f2", "fafaf9","ffffff"];
 
 export default () => (
 	<div className="colors-wrapper">
 		<section className="colors-section">
-			<h3>Colors</h3>
-			<ul>
-				<li>Primary</li>
-				<li>Secondary</li>
+			<h3 className="colors-heading">Colors</h3>
+			<ul className="colors-list">
+				{
+					colors.map((hex) => <Color hexValue={hex}/>)
+				}
 			</ul>
 		</section>
 	</div>
