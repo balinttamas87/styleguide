31d28b3f4fcc5734e98c98f0ebe93e43b7104b34
Tamas Balint <balint.tamas87@gmail.com>
2018-03-25 21:45:39 +0100

    Turn functional into class component

--- src/components/checkoutItemLine/CheckoutItemLine.js
+++ src/components/checkoutItemLine/CheckoutItemLine.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import "./CheckoutItemLine.css";
 
-export default () => (
-	<div className="checkout-basket-item-inner">
-		<p>product name</p>
-		<p>product price</p>
-		<button>-</button>
-		<input type="number"/>
-		<button>+</button>
-		<p>total value</p>
-	</div>	
-)
+class CheckoutItemLine extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalValue: 0
+    };
+
+  }
+
+  render() {
+    return (
+			<div className="checkout-basket-item-inner">
+				<p className="product-name">product name</p>
+				<p className="product-price">product price</p>
+				<button id="button-subtract" className="button-subtract">-</button>
+				<input id="product-quantity" className="product-quantity" type="number"/>
+				<button id="button-add" className="button-add">+</button>
+				<p id="total-value">total value</p>
+			</div>
+    );
+  }
+
+}
+
+export default CheckoutItemLine;
