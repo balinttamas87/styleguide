73d98ca93952be745d1ddaa0f9e9fff5751e988e
Tamas Balint <balint.tamas87@gmail.com>
2018-03-25 16:07:30 +0100

    Add spacing for Typography section
    To look more aesthetic

--- src/components/typography/Typography.js
+++ src/components/typography/Typography.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Typography.css";
+import Spacer from "../spacer/Spacer";
 
 export default () => (
 	<div className="typography-wrapper">
 		<section className="typography-section">
 			<h3>Typography</h3>
-			<ul>
+			<ul className="typography-list">
 				<li>
 					<h4>Headings</h4>
-					<p>font-family: sans-serif</p>
+					<Spacer/>
+					<p className="font-family-item">font-family: sans-serif</p>
 				</li>
+				<Spacer/>
 				<li>
 					<h4>Paragraph</h4>
-					<p>font-family: serif</p>
+					<Spacer/>
+					<p className="font-family-item">font-family: serif</p>
 				</li>
 			</ul>
 		</section>
