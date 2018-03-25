aba76e3b91cc86e8164ac55e6e1c64d98cf34a96
Tamas Balint <balint.tamas87@gmail.com>
2018-03-25 17:25:33 +0100

    Add a button for each state
    states: 'success', 'warning', 'danger'

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,5 +4,8 @@ import "./Button.css";
 export default () => (
 	<div className="button-component-wrapper">
 		<button className="button button-primary">Click</button>
+		<button className="button button-success">Click</button>
+		<button className="button button-warning">Click</button>
+		<button className="button button-danger">Click</button>
 	</div>
 )
