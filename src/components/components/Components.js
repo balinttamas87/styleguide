4cd38a3fd74ca9d7a7d63842412d8d2180d520a4
Tamas Balint <balint.tamas87@gmail.com>
2018-03-25 19:29:32 +0100

    Add basic layout for checkout

--- src/components/components/Components.js
+++ src/components/components/Components.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./Components.css";
 import Button from "../button/Button";
+import Spacer from "../spacer/Spacer";
 
 const buttonStates = ["primary","success","warning","danger"];
 
 export default () => (
 	<div className="components-wrapper">
 		<div className="buttons-wrapper">
-			<h3 className="h3">Buttons</h3>
+			<h3 className="h3 buttons-title">Buttons</h3>
 			<div className="buttons-inner">
 			{
 				buttonStates.map((state) => <Button state={state}/>)
 			}
+			</div>
+		</div>
+		<Spacer/>
+		<div className="checkout-basket-item-wrapper">
+			<h3 className="h3 checkout-item-title">Checkout item line</h3>
+			<div className="checkout-basket-item-inner">
+				<p>product name</p>
+				<p>product price</p>
+				<button>-</button>
+				<input type="number"/>
+				<button>+</button>
+				<p>total value</p>
 			</div>
 		</div>
 	</div>
